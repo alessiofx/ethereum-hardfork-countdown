96c5137c7b58d9660157e1db7970714a40ab3d3e
Alessio Pigliacelli <alessio@webok.it>
2016-11-15 18:01:26 +0100

    hardfork naumber

--- blockFork.sh
+++ blockFork.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 foo=$(curl -Ss 'https://etherchain.org/api/blocks/count' | jq '.data[0].count')
-block= expr 2642462 - $foo
+block= expr 2675000 - $foo
 echo $block
 
 
-#expr 2642462 - $(curl -Ss 'https://etherchain.org/api/blocks/count' | jq '.data[0].count')
+#expr 2675000 - $(curl -Ss 'https://etherchain.org/api/blocks/count' | jq '.data[0].count')
 
 
 
-#expr 2642462 - $(curl -Ss 'https://etherchain.org/api/blocks/count' | /usr/local/Cellar/jq/1.5_2/bin/jq '.data[0].count')
+#expr 2675000 - $(curl -Ss 'https://etherchain.org/api/blocks/count' | /usr/local/Cellar/jq/1.5_2/bin/jq '.data[0].count')
+
